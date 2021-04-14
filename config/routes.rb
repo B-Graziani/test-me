351d0b47de241480867ff023715b0957b43da0c9
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-14 20:24:29 +0200

    e

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     member do
       get :chef_name
     end
+    resources :reviews, only: [:new, :create]
   end
-  resources :reviews, only: [:new, :create]
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
